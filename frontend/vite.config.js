49da7da8675c1cad1ec92aa2d0124bc8557a075e
Otengtech <otengebenezer326@gmail.com>
2025-12-30 14:27:26 -0800

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ["react-icons/fa"]
-  },
-  base: "/"
-});
+  base: "/",
+  // This handles SPA routing during development
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
+  }
+})
